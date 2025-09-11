bcb3821cc0bb83171901ff0f4fa6d341dc2775d2
ommey <omar.aayoubii@gmail.com>
2025-09-12 01:23:42 +0200

    added tracking page from staritng project to react

--- react_exercises/react_ecommerce_vite_project/src/App.jsx
+++ react_exercises/react_ecommerce_vite_project/src/App.jsx
@@ -2,6 +2,8 @@ import { HomePage } from './pages/HomePage'
 import './App.css'
 import { Routes, Route } from 'react-router'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage' 
 
 function App() {
 
@@ -9,7 +11,8 @@ function App() {
     < Routes>
       <Route index element={< HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
-
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   )
 }
