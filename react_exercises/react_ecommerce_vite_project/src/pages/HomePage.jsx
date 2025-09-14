fb465a2941220828cd75c82131bbab14a726e384
ommey <omar.aayoubii@gmail.com>
2025-09-14 20:26:52 +0200

    fetch data from backend

--- react_exercises/react_ecommerce_vite_project/src/pages/HomePage.jsx
+++ react_exercises/react_ecommerce_vite_project/src/pages/HomePage.jsx
@@ -4,6 +4,15 @@ import './HomePage.css'
 
 
 export function HomePage() {
+
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+            
+        }).then((data) => {
+                console.log(data);
+        })
+
     return (
         <>
 
